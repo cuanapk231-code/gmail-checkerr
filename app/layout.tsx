4a3e0fe98b135db133ba84c77e9da29c27652af7
cuanapk231-code <cuanapk231@gmail.com>
2026-08-01 03:14:08 +0700

    Fix Layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,19 @@
+import type { Metadata } from 'next'
+import './globals.css'
 
+export const metadata: Metadata = {
+  title: 'Gmail Bulk Checker',
+  description: 'Cek gmail bulk',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+    }
